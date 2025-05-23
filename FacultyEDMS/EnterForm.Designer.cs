099d007574cff4924f50e1b242b4bf0e8d15a632
Matyuk <Stivano@ukr.net>
2025-05-23 18:18:54 +0300

    EnterForm grafic interface

---
+++ FacultyEDMS/EnterForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FacultyEDMS
+{
+    partial class EnterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(261, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вхід";
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.EmailBox.Location = new System.Drawing.Point(68, 43);
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(442, 26);
+            this.EmailBox.TabIndex = 1;
+            this.EmailBox.Text = "Пошта";
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.PasswordBox.Location = new System.Drawing.Point(68, 75);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(442, 26);
+            this.PasswordBox.TabIndex = 2;
+            this.PasswordBox.Text = "Пароль";
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.EnterButton.Location = new System.Drawing.Point(68, 107);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 32);
+            this.EnterButton.TabIndex = 3;
+            this.EnterButton.Text = "Вхід";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            // 
+            // EnterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 417);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.EmailBox);
+            this.Controls.Add(this.label1);
+            this.Name = "EnterForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Button EnterButton;
+    }
+}
+
--- FacultyEDMS/EnterForm.cs
+++ FacultyEDMS/EnterForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FacultyEDMS
 {
-    public partial class Form1 : Form
+    public partial class EnterForm : Form
     {
-        public Form1()
+        public EnterForm()
         {
             InitializeComponent();
         }
