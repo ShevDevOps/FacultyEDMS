e136e2b8648194068839dd585bdbbc380a4fb215
shed-crypto <alexey.rachenko@gmail.com>
2025-05-25 17:24:12 +0300

    Basic logic in UserForm and new button

--- FacultyEDMS/UserForm.Designer.cs
+++ FacultyEDMS/UserForm.Designer.cs
@@ -44,6 +44,7 @@
             this.DocumentHistoryView = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
             this.HistoryButton = new System.Windows.Forms.Button();
+            this.btnCreateNewDocument = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DocumetsView)).BeginInit();
             this.InfoBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DocumentHistoryView)).BeginInit();
@@ -53,9 +54,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(560, 31);
+            this.label1.Size = new System.Drawing.Size(706, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Особистий кабінет користувача <<USER>>\r\n";
             // 
@@ -64,38 +66,45 @@
             this.DocumetsView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.DocumetsView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DocumetsView.Location = new System.Drawing.Point(18, 75);
+            this.DocumetsView.Location = new System.Drawing.Point(24, 92);
+            this.DocumetsView.Margin = new System.Windows.Forms.Padding(4);
             this.DocumetsView.Name = "DocumetsView";
-            this.DocumetsView.Size = new System.Drawing.Size(240, 363);
+            this.DocumetsView.RowHeadersWidth = 51;
+            this.DocumetsView.Size = new System.Drawing.Size(449, 447);
             this.DocumetsView.TabIndex = 1;
             // 
             // SearchBox
             // 
             this.SearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.SearchBox.Location = new System.Drawing.Point(18, 43);
+            this.SearchBox.Location = new System.Drawing.Point(24, 53);
+            this.SearchBox.Margin = new System.Windows.Forms.Padding(4);
             this.SearchBox.Name = "SearchBox";
-            this.SearchBox.Size = new System.Drawing.Size(240, 26);
+            this.SearchBox.Size = new System.Drawing.Size(449, 30);
             this.SearchBox.TabIndex = 2;
             // 
             // SearchButton
             // 
             this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.SearchButton.Location = new System.Drawing.Point(264, 39);
+            this.SearchButton.Location = new System.Drawing.Point(480, 47);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(4);
             this.SearchButton.Name = "SearchButton";
-            this.SearchButton.Size = new System.Drawing.Size(75, 34);
+            this.SearchButton.Size = new System.Drawing.Size(100, 42);
             this.SearchButton.TabIndex = 3;
             this.SearchButton.Text = "Пошук";
             this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.radioButton1.Location = new System.Drawing.Point(264, 79);
+            this.radioButton1.Location = new System.Drawing.Point(480, 96);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(107, 24);
+            this.radioButton1.Size = new System.Drawing.Size(129, 29);
             this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
             this.radioButton1.Text = "За назвою";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
@@ -103,9 +112,10 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.radioButton2.Location = new System.Drawing.Point(264, 109);
+            this.radioButton2.Location = new System.Drawing.Point(480, 133);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(116, 24);
+            this.radioButton2.Size = new System.Drawing.Size(143, 29);
             this.radioButton2.TabIndex = 5;
             this.radioButton2.Text = "За автором";
             this.radioButton2.UseVisualStyleBackColor = true;
@@ -114,9 +124,10 @@
             // 
             this.radioButton3.AutoSize = true;
             this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.radioButton3.Location = new System.Drawing.Point(264, 139);
+            this.radioButton3.Location = new System.Drawing.Point(480, 170);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(101, 24);
+            this.radioButton3.Size = new System.Drawing.Size(121, 29);
             this.radioButton3.TabIndex = 6;
             this.radioButton3.Text = "За датою";
             this.radioButton3.UseVisualStyleBackColor = true;
@@ -124,50 +135,60 @@
             // RedactButton
             // 
             this.RedactButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.RedactButton.Location = new System.Drawing.Point(264, 169);
+            this.RedactButton.Location = new System.Drawing.Point(480, 207);
+            this.RedactButton.Margin = new System.Windows.Forms.Padding(4);
             this.RedactButton.Name = "RedactButton";
-            this.RedactButton.Size = new System.Drawing.Size(207, 34);
+            this.RedactButton.Size = new System.Drawing.Size(276, 42);
             this.RedactButton.TabIndex = 7;
             this.RedactButton.Text = "Редагувати документ";
             this.RedactButton.UseVisualStyleBackColor = true;
+            this.RedactButton.Click += new System.EventHandler(this.RedactButton_Click);
             // 
             // LookButton
             // 
             this.LookButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.LookButton.Location = new System.Drawing.Point(264, 209);
+            this.LookButton.Location = new System.Drawing.Point(480, 256);
+            this.LookButton.Margin = new System.Windows.Forms.Padding(4);
             this.LookButton.Name = "LookButton";
-            this.LookButton.Size = new System.Drawing.Size(207, 34);
+            this.LookButton.Size = new System.Drawing.Size(276, 42);
             this.LookButton.TabIndex = 8;
             this.LookButton.Text = "Переглянути документ";
             this.LookButton.UseVisualStyleBackColor = true;
+            this.LookButton.Click += new System.EventHandler(this.LookButton_Click);
             // 
             // InfoButton
             // 
             this.InfoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.InfoButton.Location = new System.Drawing.Point(264, 249);
+            this.InfoButton.Location = new System.Drawing.Point(480, 305);
+            this.InfoButton.Margin = new System.Windows.Forms.Padding(4);
             this.InfoButton.Name = "InfoButton";
-            this.InfoButton.Size = new System.Drawing.Size(207, 51);
+            this.InfoButton.Size = new System.Drawing.Size(276, 63);
             this.InfoButton.TabIndex = 9;
             this.InfoButton.Text = "переглянути інформацію про документ";
             this.InfoButton.UseVisualStyleBackColor = true;
+            this.InfoButton.Click += new System.EventHandler(this.InfoButton_Click);
             // 
             // ExitButton
             // 
             this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.ExitButton.Location = new System.Drawing.Point(345, 39);
+            this.ExitButton.Location = new System.Drawing.Point(588, 47);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(4);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(75, 34);
+            this.ExitButton.Size = new System.Drawing.Size(100, 42);
             this.ExitButton.TabIndex = 10;
             this.ExitButton.Text = "Вихід";
             this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // InfoBox
             // 
             this.InfoBox.Controls.Add(this.HideButton);
             this.InfoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.InfoBox.Location = new System.Drawing.Point(477, 169);
+            this.InfoBox.Location = new System.Drawing.Point(763, 208);
+            this.InfoBox.Margin = new System.Windows.Forms.Padding(4);
             this.InfoBox.Name = "InfoBox";
-            this.InfoBox.Size = new System.Drawing.Size(311, 269);
+            this.InfoBox.Padding = new System.Windows.Forms.Padding(4);
+            this.InfoBox.Size = new System.Drawing.Size(288, 331);
             this.InfoBox.TabIndex = 11;
             this.InfoBox.TabStop = false;
             this.InfoBox.Text = "Інформація про документ";
@@ -175,48 +196,66 @@
             // HideButton
             // 
             this.HideButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.HideButton.Location = new System.Drawing.Point(6, 25);
+            this.HideButton.Location = new System.Drawing.Point(8, 31);
+            this.HideButton.Margin = new System.Windows.Forms.Padding(4);
             this.HideButton.Name = "HideButton";
-            this.HideButton.Size = new System.Drawing.Size(175, 34);
+            this.HideButton.Size = new System.Drawing.Size(233, 42);
             this.HideButton.TabIndex = 11;
             this.HideButton.Text = "Сховати інформацію";
             this.HideButton.UseVisualStyleBackColor = true;
+            this.HideButton.Click += new System.EventHandler(this.HideButton_Click);
             // 
             // DocumentHistoryView
             // 
             this.DocumentHistoryView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.DocumentHistoryView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DocumentHistoryView.Location = new System.Drawing.Point(794, 75);
+            this.DocumentHistoryView.Location = new System.Drawing.Point(1189, 92);
+            this.DocumentHistoryView.Margin = new System.Windows.Forms.Padding(4);
             this.DocumentHistoryView.Name = "DocumentHistoryView";
-            this.DocumentHistoryView.Size = new System.Drawing.Size(240, 363);
+            this.DocumentHistoryView.RowHeadersWidth = 51;
+            this.DocumentHistoryView.Size = new System.Drawing.Size(320, 447);
             this.DocumentHistoryView.TabIndex = 12;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(790, 49);
+            this.label2.Location = new System.Drawing.Point(1053, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.Size = new System.Drawing.Size(124, 25);
             this.label2.TabIndex = 13;
             this.label2.Text = "Історія змін";
             // 
             // HistoryButton
             // 
             this.HistoryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.HistoryButton.Location = new System.Drawing.Point(264, 306);
+            this.HistoryButton.Location = new System.Drawing.Point(480, 376);
+            this.HistoryButton.Margin = new System.Windows.Forms.Padding(4);
             this.HistoryButton.Name = "HistoryButton";
-            this.HistoryButton.Size = new System.Drawing.Size(207, 33);
+            this.HistoryButton.Size = new System.Drawing.Size(276, 41);
             this.HistoryButton.TabIndex = 14;
             this.HistoryButton.Text = "переглянути історію змін";
             this.HistoryButton.UseVisualStyleBackColor = true;
+            this.HistoryButton.Click += new System.EventHandler(this.HistoryButton_Click);
+            // 
+            // btnCreateNewDocument
+            // 
+            this.btnCreateNewDocument.Location = new System.Drawing.Point(480, 424);
+            this.btnCreateNewDocument.Name = "btnCreateNewDocument";
+            this.btnCreateNewDocument.Size = new System.Drawing.Size(276, 41);
+            this.btnCreateNewDocument.TabIndex = 15;
+            this.btnCreateNewDocument.Text = "Create Document";
+            this.btnCreateNewDocument.UseVisualStyleBackColor = true;
+            this.btnCreateNewDocument.Click += new System.EventHandler(this.btnCreateNewDocument_Click);
             // 
             // UserForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1042, 450);
+            this.ClientSize = new System.Drawing.Size(1519, 554);
+            this.Controls.Add(this.btnCreateNewDocument);
             this.Controls.Add(this.HistoryButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.DocumentHistoryView);
@@ -232,6 +271,7 @@
             this.Controls.Add(this.SearchBox);
             this.Controls.Add(this.DocumetsView);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UserForm";
             this.Text = "Особистий кабінет користувача <<USER>>";
             ((System.ComponentModel.ISupportInitialize)(this.DocumetsView)).EndInit();
@@ -260,5 +300,6 @@
         private System.Windows.Forms.DataGridView DocumentHistoryView;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button HistoryButton;
+        private System.Windows.Forms.Button btnCreateNewDocument;
     }
 }
