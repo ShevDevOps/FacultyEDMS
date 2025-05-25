0c06deb5b3a23614daa2d40e152114c78dc2f205
shed-crypto <alexey.rachenko@gmail.com>
2025-05-25 17:23:26 +0300

    Check DB in EnterForm and Basic logic

--- FacultyEDMS/EnterForm.cs
+++ FacultyEDMS/EnterForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FacultyEDMS
@@ -15,6 +10,87 @@ namespace FacultyEDMS
         public EnterForm()
         {
             InitializeComponent();
+            SetupPlaceholders();
+            CheckDBStatus();
+        }
+        private void SetupPlaceholders()
+        {
+            SetPlaceholder(EmailBox, "Email");
+            SetPlaceholder(PasswordBox, "Password", true);
+        }
+        private void CheckDBStatus()
+        {
+            bool isConnected = DatabaseHelper.CheckConnection();
+            DBstat.AutoCheck = false;
+            DBstat.Checked = isConnected;
+            DBstat.ForeColor = isConnected ? Color.Green : Color.Red;
+            DBstat.Text = isConnected ? "Connected" : "No DB";
+            DBstat.FlatStyle = FlatStyle.Flat;
+            if (!isConnected)
+            {
+                MessageBox.Show("Could not connect to the database. Please check your settings.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SetPlaceholder(TextBox textBox, string placeholder, bool isPassword = false)
+        {
+            textBox.Text = placeholder;
+            textBox.ForeColor = Color.Gray;
+            if (isPassword) textBox.PasswordChar = '\0';
+
+            textBox.Enter += (sender, e) =>
+            {
+                if (textBox.Text == placeholder)
+                {
+                    textBox.Text = "";
+                    textBox.ForeColor = Color.Black;
+                    if (isPassword) textBox.PasswordChar = '*';
+                }
+            };
+
+            textBox.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    textBox.Text = placeholder;
+                    textBox.ForeColor = Color.Gray;
+                    if (isPassword) textBox.PasswordChar = '\0';
+                }
+            };
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            string email = (EmailBox.Text == "Email") ? "" : EmailBox.Text;
+            string password = (PasswordBox.Text == "Password") ? "" : PasswordBox.Text;
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter your email and password.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DataRow userData = DatabaseHelper.AuthenticateUser(email, password);
+
+            if (userData != null)
+            {
+                int userId = Convert.ToInt32(userData["id"]);
+                string username = userData["username"].ToString();
+                int role_id = Convert.ToInt32(userData["role_id"]);
+                string role_name = userData["role_name"].ToString();
+
+                MessageBox.Show($"Welcome, {username}!", "Login Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                UserForm userForm = new UserForm(userId, username, role_id, role_name);
+                this.Hide();
+                userForm.ShowDialog();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid email or password, or your account is blocked.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
